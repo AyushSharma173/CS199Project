7d3873094ba82b94c92281d639cd42e41b4f0277
Vibhav Rajkumar <vibhavr2@illinois.edu>
2021-11-18 14:05:44 -0600

    initial framework based on python script

---
+++ diffeq.rs
@@ -0,0 +1,18 @@
+extern crate ndarray;
+use pyo3::prelude::Python;
+use numpy::{ToPyArray, PyArray};
+
+struct diffeq; 
+
+impl diffeq{
+
+    pub fn RungeKutta(model: , initial_conditions: , t_initial: , t_final: , dt: )->
+    {
+
+    }
+
+    pub fn LotkaVolteraModel(x: f64, parameters: HashMap) -> {
+
+    }
+
+}
